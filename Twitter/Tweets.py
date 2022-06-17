4a5f4bbb18ac71ee6beb982650c98eabdf42fda2
amalabdulkhalig <amal4gd@gmail.com>
2022-06-16 21:10:54 -0400

    Twitter class

--- Twitter/Tweets.py
+++ Twitter/Tweets.py
@@ -3,45 +3,39 @@ import pandas as pd
 import arabic_reshaper as arabic
 
 #query used like the Twitter serch bar, a single word could be searched or 
-# you can use the advance serch then copy the query and paste it here as a text 
-class Twitter:
-    def __init__(self, query: str, limit: int) -> None:
-        self.query = query #"(from:elonmusk) until:2022-06-16 since:2020-01-01" 
-        self.tweets = [] #save the list of tweets returns 
-        self.limit = limit #10 #number of tweets returned 
-
-    def GetTweets(query):
-        # vars(tweets) returns a dictionary of the tweets any it's metadata 
-        for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-            #print(vars(tweet)) #use vars to print all attributes of a tweets
-            #break 
-            if len(tweets) == limit:
+#you can use the advance serch then copy the query and paste it here as a text 
+class Tweets:
+    def __init__(self):
+        self.query = "query" #"(from:elonmusk) until:2022-06-16 since:2020-01-01" 
+        self.tweets = list() #save the list of tweets returns 
+        self.limit = 10 #number of tweets returned 
+    
+    #-------------------------- 
+    def GetTweets(self):
+        for tweet in sntwitter.TwitterSearchScraper(self.query).get_items():
+            if len(self.tweets) == self.limit:
                 break 
             else:
-                reshaped_tweet = arabic.reshape(tweet.content)[::-1]
-                tweets.append([tweet.date,tweet.user.username,reshaped_tweet])
-
-        df = pd.DataFrame(tweets,columns=['Date','User','Tweet'])
+                self.tweets.append([tweet.date,tweet.user.username,tweet.content])
+        df = pd.DataFrame(self.tweets,columns=['Date','User','Tweet'])
         print(df['Tweet'][:1])
-
-
-    def GetArabicTweets(query):
-        # vars(tweets) returns a dictionary of the tweets any it's metadata 
-        for tweet in sntwitter.TwitterSearchScraper(query).get_items():
-            #print(vars(tweet)) #use vars to print all attributes of a tweets
-            #break 
-            if len(tweets) == limit:
-                break 
-            else:
-                reshaped_tweet = arabic.reshape(tweet.content)[::-1]
-                tweets.append([tweet.date,tweet.user.username,reshaped_tweet])
-
-        df = pd.DataFrame(tweets,columns=['Date','User','Tweet'])
-        print(df['Tweet'][:1])
+        return df
 
     #--------------------------
+    def GetArabicTweets(self):
+        for tweet in sntwitter.TwitterSearchScraper(self.query).get_items():
+            if len(self.tweets) == self.limit:
+                break 
+            else:
+                reshaped_tweet = arabic.reshape(tweet.content)[::-1]
+                self.tweets.append([tweet.date,tweet.user.username,reshaped_tweet])
 
-    def testOneTweet(query):
-        for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+        df = pd.DataFrame(self.tweets,columns=['Date','User','Tweet'])
+        print(df['Tweet'][:1])
+        return df
+
+    #--------------------------
+    def TestOneTweet(self):
+        for tweet in sntwitter.TwitterSearchScraper(self.query).get_items():
             print(vars(tweet)) #use vars to print all attributes of a tweets
             break
